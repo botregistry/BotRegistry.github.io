fce506163ce7df2e3405637d534f7da4bce99997
Alister Bulman <abulman@gmail.com>
2020-02-13 12:51:07 +0000

    Plricing/plans, links and text content

--- jekyllServe.sh
+++ jekyllServe.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
 
-docker-compose up
-
-exit
-
-./jekyll.sh serve \
---future  \
---watch   \
---drafts  \
---unpublished \
---incremental \
---host nas.abulman.co.uk \
---port 4000 \
-$@
-
-# --config _config.yml
-#        -b, --baseurl URL  Serve the website from the given base URL
-# --livereload \
+echo "You can run 'make serve' instead"
+make serve
